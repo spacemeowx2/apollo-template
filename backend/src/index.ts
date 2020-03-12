cf98b49f98ba824c4268485d3d03a1110cdd21fe
spacemeowx2 <spacemeowx2@gmail.com>
2020-03-12 19:50:15 +0800

    add apollo-server-express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,18 @@
+import 'reflect-metadata'
+import { ApolloServer, gql } from 'apollo-server-express'
+import { readFileSync } from 'fs'
+import { resolvers } from './resolvers'
 import express from 'express'
 
-const app = express()
-app.listen(8060, () => {
-  console.log('Server listening on port 8060')
-})
+async function main() {
+  const app = express()
+  const port = 8010
+  const schema = readFileSync('./schema/schema.graphql', 'utf-8')
+  const typeDefs = gql`${schema}`
+
+  const server = new ApolloServer({ typeDefs, resolvers })
+  server.applyMiddleware({ app, path: '/endpoint' })
+  app.listen(port)
+  console.log(`🚀  Server ready at http://localhost:${port}`)
+}
+main().catch(err => console.error(err))
